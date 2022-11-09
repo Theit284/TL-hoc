64a6b4c06fd60d8876721caf25d57f2f8b2a141e
Thế Lê <tienthe2973@gmail.com>
2022-11-09 19:54:23 +0700

    TL

---
+++ Thực tập/Lê TienThe-BaiTT/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ 
+    $(".owl-carousel").owlCarousel({
+   
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        nav: true,
+        dots: true,
+        pagination: true,
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+   
+    });
+   
+  });
